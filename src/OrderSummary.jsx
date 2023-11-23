24b1e7b3dab79a18ed5ba2365ce1f74d94fb22ea
Allan Musyoka <musyokaallan96@gmail.com>
2023-11-23 22:39:00 +0300

    Styled mobile and Desktop

--- src/OrderSummary.jsx
+++ src/OrderSummary.jsx
@@ -1,50 +1,56 @@
+import Hero from "./images/illustration-hero.svg";
+import MusicIcon from "./images/icon-music.svg";
 import React from 'react'
 
 function OrderSummary() {
 
     return(
 
-        <main className=''>
+        <main className='flex justify-center items-center'>
 
-            <div className=''>
+            <div className='max-w-[90%] sm:max-w-[80%] w-[500px] rounded-3xl pb-10'>
 
                 <header>
 
-                    <img src="" alt="" />
+                    <img src={Hero} alt="Hero" className="w-[100%] h-[250px] rounded-t-3xl" />
 
                 </header>
 
-                <div>
+                <div className="content px-4 sm:px-[60px]">
 
-                    <div>
+                    <div className="text-center py-10 flex flex-col gap-5">
 
-                        <h1>Order Summary</h1>
+                        <h1 className="text-[21px] sm:text-[30px]">Order Summary</h1>
 
-                        <p>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+                        <p className="text-[14px] sm:text-[18px]">You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
 
                     </div>
 
-                    <div>
+                    <div className="flex justify-between items-center pb-10">
 
-                        <img src="" alt="" />
+                        <div className="text-center flex items-center gap-2 sm:gap-8">
 
-                        <div>
+                            <img src={MusicIcon} alt="Music Icon" className="sm:w-[65px]" />
 
-                            <h2>Annual Plan</h2>
+                            <div>
 
-                            <p>$59.99/year</p>
+                                <h2 className="text-[14px] sm:text-[20px]">Annual Plan</h2>
+
+                                <p className="text-[14px] sm:text-[20px]">$59.99/year</p>
+
+                            </div>
 
                         </div>
 
-                        <p>Change</p>
+                        <p className="underline cursor-pointer text-[14px] sm:text-[20px]">Change</p>
 
                     </div>
 
                     <footer>
 
-                        <button>Proceed to Payment</button>
+                        <button className="block w-full py-4 cursor-pointer rounded-xl mb-5 sm:text-[20px]">Proceed to Payment</button>
 
-                        <button>Cancel Order</button>
+                        <button className="block w-full py-4 cursor-pointer rounded-xl sm:text-[20px]">Cancel Order</button>
 
                     </footer>
 
