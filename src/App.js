f9415d0020ae2ccbc59813fe8189c1ef32842c89
Allan Musyoka <musyokaallan96@gmail.com>
2023-11-23 21:20:43 +0300

    Added site elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import OrderSummary from './OrderSummary';
 import './App.css';
 
 function App() {
 
   return(
 
-    <h1>App</h1>
+    <OrderSummary />
 
   );
 }
---
+++ src/OrderSummary.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+function OrderSummary() {
+
+    return(
+
+        <main className=''>
+
+            <div className=''>
+
+                <header>
+
+                    <img src="" alt="" />
+
+                </header>
+
+                <div>
+
+                    <div>
+
+                        <h1>Order Summary</h1>
+
+                        <p>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+
+                    </div>
+
+                    <div>
+
+                        <img src="" alt="" />
+
+                        <div>
+
+                            <h2>Annual Plan</h2>
+
+                            <p>$59.99/year</p>
+
+                        </div>
+
+                        <p>Change</p>
+
+                    </div>
+
+                    <footer>
+
+                        <button>Proceed to Payment</button>
+
+                        <button>Cancel Order</button>
+
+                    </footer>
+
+                </div>
+
+            </div>
+
+        </main>
+
+    )
+}
+
+export default OrderSummary
